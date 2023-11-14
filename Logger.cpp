0f8975ce7a57b1d260a565a270526bfb5918aed7
Zhen Tang <silenceofdaybreak@gmail.com>
2023-11-14 09:34:25 +0800

    Add license.

--- Logger.cpp
+++ Logger.cpp
@@ -1,5 +1,12 @@
-// Author: Zhen Tang <tangzhen12@otcaix.iscas.ac.cn>
-// Affiliation: Institute of Software, Chinese Academy of Sciences
+// Copyright (c) 2023 Institute of Software, Chinese Academy of Sciences
+// libripple is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 #include <cstdio>
 #include <cstdarg>
